c8081c69c73e15c2866971f3bde02adbd5334212
leegenest <sashale21@gmail.com>
2022-01-27 21:38:49 -0500

    Welcome to the project where balls touch

--- src/main/java/io/pool/eightpool/game.java
+++ src/main/java/io/pool/eightpool/game.java
@@ -1,17 +1,77 @@
 package io.pool.eightpool;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.util.Random;
+import java.lang.Math;
 
 public class game extends Application {
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(new Pane(), 320, 240);
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 1920, 1080);
         stage.setTitle("EightPool");
+
+        Rectangle table = new Rectangle(1080, 720);
+        table.setFill(Color.GREEN);
+        table.setX(300);
+        table.setY(100);
+        root.getChildren().addAll(table);
+
+        Circle ball1 = new Circle();
+        ball1.setRadius(30);
+        //ball1.setFill(Color.WHITE);
+        ball1.setLayoutX(700);
+        ball1.setLayoutY(400);
+        root.getChildren().addAll(ball1);
+
+
+        Circle ball2 = new Circle();
+        ball2.setRadius(30);
+        ball2.setFill(Color.RED);
+        ball2.setLayoutX(900);
+        ball2.setLayoutY(200);
+        root.getChildren().addAll(ball2);
+
+
+        double distance;
+
+
+        for(int i = 0; i<1000; i++){
+            //System.out.println(i);
+            distance = Math.sqrt((Math.pow(ball1.getLayoutX()-ball2.getLayoutX(), 2) + Math.pow(ball1.getLayoutX()-ball2.getLayoutX(), 2))) ;
+            System.out.println(distance);
+
+            if((distance<= ball1.getRadius()*2)){
+            //if(Math.abs(ball1.getLayoutX()-ball2.getLayoutX()) <=ball1.getRadius()*2 || Math.abs(ball1.getLayoutY()-ball2.getLayoutY()) <=ball1.getRadius()*2) {
+
+                System.out.println(ball1.getLayoutX());
+                System.out.println(ball2.getLayoutX());
+                System.out.println(ball1.getLayoutY());
+                System.out.println(ball2.getLayoutY());
+                System.out.println(i);
+                break;
+
+
+            }
+            ball1.setLayoutX(ball1.getLayoutX()+1);
+            ball1.setLayoutY(ball1.getLayoutY()-1);
+
+        }
+
+
+
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
