0141dd5fc117bc5e54b00f6e510c835c874124c9
Zeiad Badawy <2055037@edu.vaniercollege.qc.ca>
2022-02-02 20:02:52 -0500

    Added new properties to VelocityVector

--- src/main/java/io/pool/model/VelocityVector.java
+++ src/main/java/io/pool/model/VelocityVector.java
@@ -3,6 +3,10 @@ package io.pool.model;
 public class VelocityVector {
     private double x;
     private double y;
+    // TODO I just added these. I think they will be useful
+    private double angle;
+    private double vector;
+    private double magnitude;
 
     public VelocityVector(double x, double y) {
         this.x = x;
