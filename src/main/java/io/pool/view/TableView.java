ba6823275beb577bcda4aed8b99d0c1c5f6e6f22
Abdoul111 <93938245+Abdoul111@users.noreply.github.com>
2022-02-03 11:34:05 -0500

    adding an imageView for the table, with many modifications on the project

--- src/main/java/io/pool/view/TableView.java
+++ src/main/java/io/pool/view/TableView.java
@@ -4,7 +4,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 import java.io.File;
